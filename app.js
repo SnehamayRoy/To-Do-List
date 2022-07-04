c3a10892a72e5925f38b0fa960f99e7216bf7aa6
Snehamay Roy <roysnehamay@gmail.com>
2022-07-04 21:48:47 +0530

    Small Change

--- app.js
+++ app.js
@@ -163,12 +163,12 @@ app.post("/delete" ,(req,res)=>{
 //   }
 // })
 
-// const port=process.env.PORT;
-// if(port==null||port==""){
-//   port=3000
+const port=process.env.PORT;
+if(port==null||port==""){
+  port=3000
 
-// }
+}
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("Server is runnig "); 
 })
